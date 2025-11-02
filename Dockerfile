f6b099f63503df3fb5166670bc95dfd8f799faf7
cmlsn <100160785+cmlsn@users.noreply.github.com>
2025-11-02 12:13:25 -0800

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,37 @@
+# docker build -t ff .
+# docker run -p 3000:3000 -it ff
+# Build stage
+FROM node:22-slim AS builder
+
+WORKDIR /gen3
+
+COPY ./package.json ./package-lock.json ./next.config.js ./tsconfig.json ./.env.development  ./tailwind.config.js ./postcss.config.js ./start.sh ./.env.production ./
+RUN npm ci
+COPY ./src ./src
+COPY ./public ./public
+COPY ./config ./config
+COPY ./start.sh ./
+RUN npm install @swc/core @napi-rs/magic-string && \
+    npm run build
+
+# Production stage
+FROM node:22-slim AS runner
+
+WORKDIR /gen3
+
+RUN addgroup --system --gid 1001 nextjs && \
+    adduser --system --uid 1001 nextjs
+
+COPY --from=builder /gen3/package.json ./
+COPY --from=builder /gen3/node_modules ./node_modules
+COPY --from=builder /gen3/config ./config
+COPY --from=builder /gen3/.next ./.next
+COPY --from=builder /gen3/public ./public
+COPY --from=builder /gen3/start.sh ./start.sh
+RUN mkdir -p /gen3/.next/cache/images
+RUN chmod -R 777 /gen3/.next/cache
+RUN chown nextjs:nextjs /gen3/.next
+
+USER nextjs:nextjs
+ENV PORT=3000
+CMD bash ./start.sh
---
+++ src/pages/_app.tsx
@@ -0,0 +1,128 @@
+import App, { AppProps, AppContext, AppInitialProps } from 'next/app';
+import React, { useState, useEffect, useRef, Suspense } from 'react';
+import { MantineProvider } from '@mantine/core';
+import mantinetheme from '../mantineTheme';
+
+import {
+  Gen3Provider,
+  type ModalsConfig,
+  RegisteredIcons,
+  SessionConfiguration,
+  registerCohortDiscoveryApp,
+  registerExplorerDefaultCellRenderers,
+  registerCohortBuilderDefaultPreviewRenderers,
+  registerMetadataSchemaApp,
+} from '@gen3/frontend';
+
+import { registerCohortTableCustomCellRenderers } from '@/lib/CohortBuilder/CustomCellRenderers';
+import { registerCustomExplorerDetailsPanels } from '@/lib/CohortBuilder/FileDetailsPanel';
+
+import '../styles/globals.css';
+import '@fontsource/montserrat';
+import '@fontsource/source-sans-pro';
+import '@fontsource/poppins';
+
+import { setDRSHostnames, registerDefaultRemoteSupport } from '@gen3/core';
+import drsHostnames from '../../config/drsHostnames.json';
+import { loadContent } from '@/lib/content/loadContent';
+import Loading from '../components/Loading';
+import DatadogInit from '@/components/DatadogInit';
+
+if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'production') {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-require-imports
+  const ReactDOM = require('react-dom');
+  // eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-require-imports
+  const axe = require('@axe-core/react');
+  axe(React, ReactDOM, 1000);
+}
+
+interface Gen3AppProps {
+  icons: Array<RegisteredIcons>;
+  modalsConfig: ModalsConfig;
+  sessionConfig: SessionConfiguration;
+}
+
+const Gen3App = ({
+  Component,
+  pageProps,
+  icons,
+  sessionConfig,
+  modalsConfig,
+}: AppProps & Gen3AppProps) => {
+  const isFirstRender = useRef(true);
+
+  useEffect(() => {
+    if (isFirstRender.current) {
+      setDRSHostnames(drsHostnames);
+      registerDefaultRemoteSupport();
+      registerMetadataSchemaApp();
+      registerCohortDiscoveryApp();
+      registerExplorerDefaultCellRenderers();
+      registerCohortBuilderDefaultPreviewRenderers();
+      registerCohortTableCustomCellRenderers();
+      registerCustomExplorerDetailsPanels();
+      isFirstRender.current = false;
+      console.log('Gen3 App initialized');
+    }
+  }, []);
+
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true); // Only on client-side
+  }, []);
+  return (
+    <React.Fragment>
+      {isClient ? (
+        <Suspense fallback={<Loading />}>
+          <DatadogInit />
+            <MantineProvider theme={mantinetheme}>
+              <Gen3Provider
+                icons={icons}
+                sessionConfig={sessionConfig}
+                modalsConfig={modalsConfig}
+              >
+                <Component {...pageProps} />
+              </Gen3Provider>
+            </MantineProvider>
+        </Suspense>
+      ) : (
+        // Show some fallback UI while waiting for the client to load
+        <Loading />
+      )}
+    </React.Fragment>
+  );
+};
+
+// TODO: replace with page router
+Gen3App.getInitialProps = async (
+  context: AppContext,
+): Promise<Gen3AppProps & AppInitialProps> => {
+  const ctx = await App.getInitialProps(context);
+
+  try {
+    const res = await loadContent();
+    return {
+      ...ctx,
+      ...res,
+    };
+  } catch (error: any) {
+    console.error('Provider Wrapper error loading config', error.toString());
+  }
+  // return default
+  return {
+    ...ctx,
+    icons: [
+      {
+        prefix: 'gen3',
+        lastModified: 0,
+        icons: {},
+        width: 0,
+        height: 0,
+      },
+    ],
+    modalsConfig: {},
+    sessionConfig: {},
+  };
+};
+export default Gen3App;
